5f301d6e123d262e565618d44d50cade29288385
Mounir Bkr <miirbri1@gmail.com>
2022-12-05 22:59:32 +0100

    commit final "security forms commit final works good"

---
+++ src/test/java/com/mnr/springsecurityforms/SpringsecurityFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.springsecurityforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
